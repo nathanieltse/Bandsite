b8c4152a6946c13a9ed770bd88cceab00aab61d6
nathanieltse <nathanieltse@hotmail.com>
2021-06-02 17:48:09 -0400

    sprint 3 first commit

---
+++ sprint-three/scripts/build-shows-page.js
@@ -0,0 +1,89 @@
+//all data list
+const dateList = ["Mon Sept 06 2021", "Tue Sept 21 2021", "Fri Oct 15 2021", "Sat Nov 06 2021", "Fri Nov 26 2021", "Wed Dec 15 2021"]
+const venueList = ["Ronald Lane", "Pier 3 East", "View Lounge", "Hyatt Agency", "Moscow Center", "Press Club"]
+
+
+//function for adding element
+const addElement = (element, className) => {
+    return (content) => {
+        const el = document.createElement(element)
+        el.className = className
+        el.innerHTML = content
+        return el
+    }
+}
+
+//selecting body 
+const main = document.querySelector('.main')
+
+//adding title
+let title = addElement("h2", "main__title")
+title = title("Shows")
+main.append(title)
+
+
+//subtitle for tablet and desktop
+let mainBody = addElement("section", "main__body")
+let titleContainer = addElement("ul", "main__tablet-subtitle-box")
+let dateTitle = addElement("li", "main__tablet-subtitle")
+let venueTitle = addElement("li", "main__tablet-subtitle")
+let locationTitle = addElement("li", "main__tablet-subtitle")
+let lastBox = addElement("li", "main__tablet-subtitle")
+
+//set default text
+mainBody = mainBody("")
+titleContainer = titleContainer("")
+dateTitle = dateTitle("DATES")
+venueTitle =venueTitle("VENUE")
+locationTitle = locationTitle("LOCATION")
+lastBox = lastBox("")
+
+
+//add all elements to container
+titleContainer.append(dateTitle, venueTitle, locationTitle,lastBox)
+mainBody.append(titleContainer)
+main.append(mainBody)
+
+dateList.forEach((element, index) => {
+
+    //variable for new elements
+    let infoCard = addElement('ul', "card")
+    let dateTitle = addElement('li', "card__subtitle")
+    let dateText = addElement('li', "card__date")
+    let venueTitle = addElement('li', "card__subtitle")
+    let venueText = addElement('li', "card__text")
+    let locationTitle = addElement('li', "card__subtitle")
+    let locationText = addElement('li', "card__text")
+    let buyBtn = addElement('button', "button")
+
+    //setting content
+    infoCard = infoCard("")
+    dateTitle = dateTitle("DATES")
+    venueTitle = venueTitle("VENUE")
+    locationTitle = locationTitle("LOCATION")
+    locationText = locationText("San Francisco, CA")
+    buyBtn = buyBtn("BUY TICKETS")
+    dateText = dateText(dateList[index])
+    venueText = venueText(venueList[index])
+
+    //adding all data to HTML
+    
+    infoCard.append(dateTitle,dateText,venueTitle,venueText,locationTitle,locationText,buyBtn)
+    mainBody.append(infoCard)
+
+})
+
+//button function
+document.querySelectorAll(".button").forEach(button => {
+    button.addEventListener("click", (e) => buttonFunction(e))
+})
+
+const buttonFunction = (e) => {
+    const location = e.target.parentNode.querySelectorAll('.card__text')
+    const address = ["Venue location: "]
+    location.forEach(tag => {
+        address.push(tag.textContent)
+    })
+    console.log(address.join(' '))
+}
+
---
+++ sprint-three/scripts/index-page.js
@@ -0,0 +1,163 @@
+//all comments
+comments = [
+    {
+        fullName:"Connor Walton",
+        timestamp:"02/17/2021",
+        comment:"This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
+    },
+    {
+        fullName:"Emilie Beach",
+        timestamp:"01/09/2021",
+        comment:"I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
+    },
+    {
+        fullName:"Miles Acosta",
+        timestamp:"12/20/2020",
+        comment:"I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
+    }
+]
+
+//function for adding element
+const addElement = (element, className) => {
+    return (content) => {
+        const el = document.createElement(element)
+        el.className = className
+        el.innerHTML = content
+        return el
+    }
+}
+
+const addEmptyElement = (element, className) => {
+    const el = document.createElement(element)
+    el.className = className
+    return el
+}
+
+//function for displaying comment
+const displayComment = (comment) => {
+    //all new elements
+    const commentBody = addEmptyElement("section", "comment__body")
+    const commentCard = addEmptyElement("article", "comment__card")
+    const avatar = addEmptyElement("div", "comment__avatar")
+    const commentSubWrapper = addEmptyElement("div", "comment__sub-wrapper")
+    let commentName = addElement("p","comment__name")
+    let commentText = addElement("p", "comment__text")
+    let commentDate = addElement("p", "comment__date")
+
+    //adding text
+    commentName = commentName(comment.fullName)
+    commentText = commentText(comment.comment)
+    commentDate = commentDate(dateConvert(comment.timestamp))
+
+    // adding all elements together
+    commentSubWrapper.append(commentName, commentDate,commentText)
+    commentCard.append(avatar, commentSubWrapper)
+    commentBody.append(commentCard)
+    commentWrapper.append(commentBody)
+
+}
+
+//function for converting timestamp
+const dateConvert = (date) => {
+    const today = new Date()
+
+    const pastMonth = Number(date.slice(0,2))-1
+    const pastDay = Number(date.slice(3,5))
+    const pastYear = Number(date.slice(6,10))
+    const past = new Date(pastYear,pastMonth, pastDay)
+
+    //calculate millisecond to day difference
+    let difference = today.getTime() - past.getTime()
+    difference = difference/(24*60*60*1000)
+    // difference = 0.5
+
+    if (difference < 1){
+        return "today"
+    } else {
+        return `${parseInt(difference)} days ago`
+    }
+
+}
+
+//selecting body 
+const main = document.querySelector('.main')
+
+//variable for new elements
+const commentcontainer = addEmptyElement("section", "comment")
+const commentWrapper = addEmptyElement("div", "comment__wrapper")
+const commentForm = addEmptyElement("section", "comment__form")
+const form = addEmptyElement("form", "form")
+const avatar = addEmptyElement("div", "form__avatar")
+const profile = addEmptyElement("img","form__profile")
+const inputWrapper = addEmptyElement("div", "form__wrapper")
+const nameBox = addEmptyElement("input", "form__nameinput")
+const commentBox = addEmptyElement("textarea", "form__commentinput")
+let header = addElement("h2", "comment__title")
+let nameLabel = addElement("label", "form__label")
+let commentLabel = addElement("label","form__label")
+let submit = addElement("button","form__submit")
+
+//adding text
+header = header("Join the Conversation")
+nameLabel = nameLabel("NAME")
+commentLabel = commentLabel("COMMENT")
+submit = submit("COMMENT")
+
+//all attributes
+profile.src = "./assets/images/Mohan-muruge.jpg"
+profile.alt = "photo of monha muruge"
+nameLabel.htmlFor = "fullName"
+nameBox.name = "fullName"
+nameBox.placeholder = "Enter your name"
+nameBox.required = true
+commentLabel.htmlFor = "comment"
+commentBox.name = "comment"
+commentBox.placeholder = "Add a new comment"
+commentBox.required = true
+submit.type="submit"
+
+
+//adding all elements together
+avatar.append(profile)
+inputWrapper.append(nameLabel,nameBox,commentLabel,commentBox,submit)
+form.append(avatar,inputWrapper)
+commentForm.append(form)
+commentWrapper.append(header,commentForm)
+commentcontainer.append(commentWrapper)
+main.append(commentcontainer)
+
+//displaying all comments
+comments.forEach(comment => {
+    displayComment(comment)
+});
+
+
+//form submit
+document.querySelector(".form").addEventListener("submit",(e)=>{
+    e.preventDefault()
+    //date contructor
+    const today = new Date()
+    const month = ("0" + (today.getMonth()+1)).slice(0,2)
+    //adding data to comments array
+    comments.unshift({
+        fullName: e.target.fullName.value,
+        timestamp:`${month}/${today.getDate()}/${today.getFullYear()}`,
+        comment: e.target.comment.value
+    })
+    //removing all comments and display all comments
+    document.querySelectorAll(".comment__body").forEach(element =>{
+        element.remove()
+    })
+    comments.forEach(comment => {
+        displayComment(comment)
+    });
+    document.querySelector(".comment__card").classList.add("comment__body--new")
+    const newElProfile = addEmptyElement("img","form__profile")
+    document.querySelector(".comment__card .comment__avatar").append(newElProfile)
+    newElProfile.src = "./assets/images/Mohan-muruge.jpg"
+    newElProfile.alt = "photo of monha muruge"
+
+    //clearing form
+    e.target.reset()
+    
+})
